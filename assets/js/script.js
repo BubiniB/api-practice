44bd904690c47d576f461d027ec41d3ce7856ff6
BubiniB <cristina.ekqvist@outlook.com>
2023-07-10 09:04:52 +0000

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const API_KEY = "14CYosji3U0VQn21FV7mczUZD0M";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
